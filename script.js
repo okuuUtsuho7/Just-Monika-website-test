435327917144a58fa38e70a4a9290eff90bd52e8
RPGMN777 <89574595+RPGMN777@users.noreply.github.com>
2022-02-22 09:08:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// This is the scripts file
+console.log("This is the console log")
+
+document.getElementById("btn1").onclick = function() {
+	console.log("Monika loves you too!")
+}
+
+document.getElementById("btn2").onclick = function(){
+	console.log("Well you're next to die")
+}
+
+document.getElementById("btn3").onclick = function() {
+	console.log("Monika is upset")
+}
